7d221e4cf564c39df34d4fd0be865d26e6afc9ab
nhess1981 <nhess@opentext.com>
2015-12-22 11:07:21 +0100

    Update CreateCSimage.ps1

--- CreateCSimage.ps1
+++ CreateCSimage.ps1
@@ -2,39 +2,42 @@ param(
 [String]$ReplaceDBServer,
 [String]$ReplaceDBName,
 [String]$ReplaceDBUser,
-[String]$ReplaceDBPassword)
+[String]$ReplaceDBPassword,
+[String]$ReplaceAPassword)
+
+Write-Host "Start: Replace Parameter in autoconfig.xml"
 
 $file = "C:\OPENTEXT\config\autoconfig.xml"
 $StrDBServer = "DBServer"
 $StrDBName = "DBName"
 $StrDBUser = "DBUser"
 $StrDBPassword = "DBPassword"
+$StrAPassword = "APassword"
 
 $ReplaceDBServerFull = $ReplaceDBServer+".database.windows.net"
-
-#$ReplaceDBServer = "otcs.database.windows.net"
-#$ReplaceDBName = "otcsps"
-#$ReplaceDBUser = "otcsps"
-#$ReplaceDBPassword = "Livelink1!"
 
 Write-Host $ReplaceDBServerFull
 Write-Host $ReplaceDBName
 Write-Host $ReplaceDBUser
 Write-Host $ReplaceDBPassword
+Write-Host $ReplaceAPassword
 
-$text = get-content $file
-$newText0 = $text -replace $StrDBServer,$ReplaceDBServerFull
-$newText1 = $newText0 -replace $StrDBName,$ReplaceDBName
-$newText2 = $newText1 -replace $StrDBUser,$ReplaceDBUser
-$newText3 = $newText2 -replace $StrDBPassword,$ReplaceDBPassword
-$newText3 > $file 
+(Get-Content $file) -replace $StrDBServer, $ReplaceDBServerFull | Set-Content $file
+(Get-Content $file) -replace $StrDBName, ,$ReplaceDBName | Set-Content $file
+(Get-Content $file) -replace $StrDBUser, $ReplaceDBUser | Set-Content $file
+(Get-Content $file) -replace $StrDBPassword, $ReplaceDBPassword | Set-Content $file
+(Get-Content $file) -replace $StrAPassword, $ReplaceAPassword | Set-Content $file
+
+Write-Host "End: Replace Parameter in autoconfig.xml"
 
 # Start Services
 cmd.exe /c '\configOTCS.bat'
-# change to the path to IE on your machine if necessary
-$IE = "C:\Program Files\Internet Explorer\iexplore.exe"
-# I created a sample XML file for you and put it on my server; you can try the URL below
-# But you will have to change it if you want to open another file
-$XML = "http://localhost/OTCS/cs.exe?func=admin.autoconfig"
-# Lanch Internet Explorer and open an XML file in it
-Start-Process $IE $XML
+
+Write-Host "Start: auto creation Content Server"
+
+$url = "http://localhost/OTCS/cs.exe?func=admin.autoconfig"
+$result = Invoke-WebRequest $url
+
+Write-Host $result
+
+Write-Host "End: auto creation Content Server"
