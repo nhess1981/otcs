99fff4a986dc7c4fa7b2e53a6abe26c424dd0355
nhess1981 <nhess@opentext.com>
2015-12-29 16:38:54 +0100

    Update CreateCSimage.ps1

--- CreateCSimage.ps1
+++ CreateCSimage.ps1
@@ -50,7 +50,7 @@ Write-Host "Start: auto creation Content Server"
 
 $url = "http://localhost/OTCS/cs.exe?func=admin.autoconfig"
 
-$result = Invoke-WebRequest $url -TimeoutSec 360 -UseBasicParsing
+$result = Invoke-WebRequest $url -UseBasicParsing
 
 Write-Host $result
 
